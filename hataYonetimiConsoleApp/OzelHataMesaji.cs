9f7746eea8c0a377f87b625e6bdb8accbbc7ca1d
Murat Karasah <mkarasah@gmail.com>
2021-12-03 09:23:37 +0300

    Add project files.

---
+++ hataYonetimiConsoleApp/OzelHataMesaji.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace hataYonetimiConsoleApp
+{
+    public class OzelHataMesaji:Exception
+    {
+        public OzelHataMesaji()
+        {
+            Console.WriteLine("Özel Hata Mesajı");
+
+        }
+    }
+}
---
+++ hataYonetimiConsoleApp/Program.cs
@@ -0,0 +1,33 @@
+﻿using System;
+
+namespace hataYonetimiConsoleApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            try
+            {
+                Console.WriteLine("bir sayı giriniz");
+                int enter = Convert.ToInt32(Console.ReadLine());
+                if (enter==10)
+                {
+                    throw new OzelHataMesaji();
+                }
+            }
+            catch (OzelHataMesaji fx)
+            {
+                Console.WriteLine("Hatalı format özel hata mesajı girdi");
+            }
+            catch (FormatException)
+            {
+                Console.WriteLine("Hatalı format");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine("Lütfen sayı giriniz...{0}",ex);
+            }
+        }
+    }
+}
